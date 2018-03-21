bb49451fbfac961412f81fe4252bfbbaf5281990
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-21 01:25:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@
 class CashRegister
   attr_accessor :total, :discount
 
-  def initialize(discount = 0)
+  def initialize(discount = 0.0)
     @total = 0.0
     @discount = discount.to_f
   end
@@ -13,8 +13,9 @@ class CashRegister
   end
 
   def apply_discount
-    self.total = self.total * (1.0 - self.discount/100.0)
-    "After the discount, the total comes to $#{self.total.to_i}."
+    if
+      self.total = self.total * (1.0 - self.discount/100.0)
+      "After the discount, the total comes to $#{self.total.to_i}."
   end
 
   def self.void_last_transaction
