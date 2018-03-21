090918bc511f1246b8e92c80c9b5ac7567390a5a
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-21 01:41:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ class CashRegister
 
   def void_last_transaction
 
-    quantity.times{ self.items << title }
+    # quantity.times{ self.items << title }
     self.total -= last_transaction[price] * last_transaction[quantity]
 
   end
