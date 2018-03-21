66eb3496989ee71b2ad5c16c55b1c3a249b38406
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-21 01:18:27 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
 
   def apply_discount
-    self.total = self.total * (1 - self.discount)
+    self.total = self.total * (1.0 - self.discount/100)
   end
 
   def self.void_last_transaction
